7b7c049ef65a2d4051680da7c91722d29842b686
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2023-03-04 23:52:20 +0100

    Prettier was added to the project.

--- src/main/java/daw/pr/minesweeper/App.java
+++ src/main/java/daw/pr/minesweeper/App.java
@@ -5,38 +5,23 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
-
 public class App {
-    private static final Logger logger = LogManager.getLogger(App.class);
 
-    public static void main(String[] args) {
+  private static final Logger logger = LogManager.getLogger(App.class);
 
-        // Check if the String[] args contains the debug flag (--debug or -d)
-        // if other than --debug -d or nothing is display, raise and exception
-        // and exit the program
-        if (args.length > 0) {
-            if (args[0].equals("--debug") || args[0].equals("-d")) {
-                Configurator.setRootLevel(Level.DEBUG);
-                logger.info("Debug mode enabled");
-            } else {
-                logger.error("Invalid argument: " + args[0]);
-                logger.error("Usage: java -jar minesweeper.jar [--debug|-d]");
-                System.exit(1);
-            }
-        } else {
-            logger.info("Release mode enabled");
-            Configurator.setRootLevel(Level.ERROR);
-        }
+  public static void main(String[] args) {
+    // Check if the String[] args contains the debug flag (--debug or -d)
+    // if other than --debug -d or nothing is display, raise and exception
+    // and exit the program
 
-        logger.info("Starting Minesweeper");
-        logSystemInfo();
-        logger.error("Error message");
+    logger.info("Starting Minesweeper");
+    logSystemInfo();
+    logger.error("Error message");
+  }
 
-    }
-
-    private static void logSystemInfo() {
-        logger.info("Java System Properties:");
-        System.getProperties().forEach((k, v) -> logger.info(k + " = " + v));
-        logger.info("End of Java System Properties");
-    }
+  private static void logSystemInfo() {
+    logger.info("Java System Properties:");
+    System.getProperties().forEach((k, v) -> logger.info(k + " = " + v));
+    logger.info("End of Java System Properties");
+  }
 }
