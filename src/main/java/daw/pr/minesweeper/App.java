b2b37569c54b130360f8b6ca8c15b340803ca574
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2023-03-04 23:14:20 +0100

    Solved an issue with a broken dependency in Maven. Bumped log4j2 to 2.20.0.

--- src/main/java/daw/pr/minesweeper/App.java
+++ src/main/java/daw/pr/minesweeper/App.java
@@ -5,9 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
-/**
- * Hello world!
- */
+
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class);
 
