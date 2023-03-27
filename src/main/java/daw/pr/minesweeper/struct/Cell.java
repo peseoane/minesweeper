33c21050f6e1a5e5123334eced2fb51808ccc23d
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2023-03-27 19:44:16 +0200

    Removed obsolete JavaDoc.

--- src/main/java/daw/pr/minesweeper/struct/Cell.java
+++ src/main/java/daw/pr/minesweeper/struct/Cell.java
@@ -5,11 +5,7 @@ public class Cell {
     private State state;
     private Difficulty difficulty;
 
-    /**
-     * Crea una celda
-     *
-     * @param int Difficulty de 0 a 3
-     */
+
     public Cell(Difficulty difficulty) {
         this.state = State.HIDDEN;
         this.difficulty = difficulty;
