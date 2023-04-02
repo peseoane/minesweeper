7ba8bb339177ba56797a19e3d4513479dd6d3783
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2023-04-02 18:49:11 +0200

    Basic spring boot config added in maven.

---
+++ src/main/java/daw/pr/minesweeper/MinesweeperApplication.java
@@ -0,0 +1,13 @@
+package daw.pr.minesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daw/pr/minesweeper/MinesweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.pr.minesweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
