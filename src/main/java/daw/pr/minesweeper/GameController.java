9f09469e1a6d8e9ddec30550acb9be9d155b6381
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2023-04-02 21:26:30 +0200

    WIP (not working mouse listener)

--- src/main/java/daw/pr/minesweeper/GameController.java
+++ src/main/java/daw/pr/minesweeper/GameController.java
@@ -1,53 +1,70 @@
 package daw.pr.minesweeper;
 
-import daw.pr.minesweeper.struct.Cell;
-import daw.pr.minesweeper.struct.Difficulty;
-import daw.pr.minesweeper.struct.Game;
-import daw.pr.minesweeper.struct.StateSelf;
+import daw.pr.minesweeper.struct.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GameController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GameController.class);
+    Game game;
 
     @GetMapping("/startGame")
     @ResponseBody
     public String minesweeper(@RequestParam(value = "difficulty") String difficultyString) {
         LOGGER.info("Starting Minesweeper with difficulty: " + difficultyString);
         Difficulty difficulty = Difficulty.valueOf(difficultyString.toUpperCase());
-        Game game = new Game(difficulty);
+        game = new Game(difficulty);
+        LOGGER.debug(game.toString());
+        return getTableHtml();
+    }
 
-        int rows = difficulty.getRows();
-        int columns = difficulty.getColumns();
+    @GetMapping("/revealCell")
+    @CrossOrigin(origins = "http://localhost:8080")
+    public String revealCell(@RequestParam("row") int row, @RequestParam("column") int column) {
+        LOGGER.info("Revealing cell: " + row + ", " + column);
+        if (game == null) {
+            LOGGER.error("Game object is null!");
+        }
+        game.getCell(row, column).setStateCanvas(StateCanvas.REVEALED);
+        // crear y devolver la tabla actualizada
+        return getTableHtml();
+    }
+
+
+    private String getTableHtml() {
         int totalCells = game.getTotalCells();
-
         StringBuilder sb = new StringBuilder();
         sb.append("<h1>Minesweeper</h1>");
+        sb.append("<link rel='stylesheet' href='./style.css'>");
+        sb.append("<script src='./app.js'></script>");
+        sb.append("<div id='table-container'>");
         sb.append("<table>");
-
         for (Cell[] row : game.getCells()) {
             sb.append("<tr>");
+
             for (Cell cell : row) {
                 sb.append("<td>");
                 if (cell.getStateSelf() == StateSelf.MINE) {
-                    sb.append("* ");
+                    sb.append("<div class='mine'>.</div> ");
+                } else if (cell.getStateCanvas() == StateCanvas.HIDDEN) {
+                    sb.append("<div class='hidden' onclick='revealCell(" + cell.getRow() + ", " + cell.getColumn() +
+                                      ")'>_</div> ");
                 } else {
-                    sb.append(cell.getMinesAround() + " ");
+                    sb.append("<div class='visible'>" + cell.getMinesAround() + "</div> ");
                 }
                 sb.append("</td>");
             }
             sb.append("</tr>");
         }
-
         sb.append("</table>");
-        sb.append("<p>Remaining cells: ").append(totalCells).append("</p>");
+        sb.append("</div>");
+        sb.append("<p>Remaining cells:</p>");
+        sb.append("<p id='remaining-cells'>" + game.getTotalCells() + "</p>");
 
         return sb.toString();
     }
+
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,10 +1,25 @@
 var tableContainer = document.getElementById('table-container');
 
-// obtener el contenido de la tabla desde el servidor
-fetch('/startGame?difficulty=EASY')
-    .then(response => response.text())
-    .then(tableHtml => {
-        // agregar la tabla al elemento con el ID 'table-container'
-        tableContainer.innerHTML = tableHtml;
-    })
-    .catch(error => console.error(error));
+const tbody = document.querySelector('#table-container tbody');
+tbody.addEventListener('click', function (e) {
+    const cell = e.target.closest('td');
+    if (!cell) {
+        return;
+    } // Quit, not clicked on a cell
+    const row = cell.parentElement;
+    console.log(cell.innerHTML, row.rowIndex, cell.cellIndex);
+    cell.classList.remove('hidden');
+    if (cell.innerHTML === '.') {
+        cell.classList.add('mine-clicked');
+    }
+});
+
+function revealCell(row, column) {
+    console.log('Clicked on cell (' + row + ', ' + column + ')');
+    fetch('/revealCell?row=' + row + '&column=' + column)
+        .then(response => response.text())
+        .then(tableHtml => {
+            tableContainer.innerHTML = tableHtml;
+        })
+        .catch(error => console.error(error));
+}
